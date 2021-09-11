5b8ad5ed303454e30956730ae8d55e8a11d92aec
Ivan Yakimov <edlinsoft@gmail.com>
2021-09-11 19:38:24 +0300

    Slideshow instruments

--- src/client/components/slideshow.jsx
+++ src/client/components/slideshow.jsx
@@ -1,61 +1,114 @@
-import React, { useEffect, useRef } from 'react'
-import { connect } from 'react-redux'
-import Header from './header.jsx'
-import Searcher from './searcher.jsx';
-import LoadingIndicator from './loading-indicator.jsx'
-import {
-    showSlide
-  } from "../actions.js";
+import React, { useEffect, useRef, useState } from "react";
+import { connect } from "react-redux";
+import Header from "./header.jsx";
+import Searcher from "./searcher.jsx";
+import LoadingIndicator from "./loading-indicator.jsx";
+import { showSlide } from "../actions.js";
 
+export const SlideShow = ({ slideImage, getSlide }) => {
+  const timerRef = useRef(null);
 
-export const SlideShow = ({slideImage, getSlide}) => {
+  const disableTimer = () => {
+    if (!!timerRef.current) {
+      clearTimeout(timerRef.current);
+      timerRef.current = null;
+    }
+  };
 
-    const timerRef = useRef(null);
+  const [slidesInterval, setSlidesInterval] = useState(10000);
 
-    useEffect(() => {
-        if(!slideImage)
-        {
-            getSlide();
-        }
-        else
-        {
-            if(!!timerRef.current) {
-                clearTimeout(timerRef.current);
-            }
+  const [isPaused, setIsPaused] = useState(false);
 
-            const timerId = setTimeout(() => {
-                getSlide();
-            }, 10000);
+  useEffect(() => {
+    if (!slideImage) {
+      getSlide();
+    } else {
+      disableTimer();
 
-            timerRef.current = timerId;
-        }
-    });
+      if (isPaused) {
+        return;
+      }
 
-    return (<div className="container">
-        <Header />
-        <Searcher />
-        {
-            !slideImage
-                ? <LoadingIndicator loading={true} />
-                : (<div className="imageDiv">
-                    <img className="full-image" src={slideImage} />
-                </div>)
-        }
-    </div>);
-}
+      const timerId = setTimeout(() => {
+        getSlide();
+      }, slidesInterval);
+
+      timerRef.current = timerId;
+    }
+  });
+
+  const handleTimeoutChange = (event) => {
+    disableTimer();
+
+    const newTimeout = parseInt(event.target.value, 10);
+
+    setSlidesInterval(newTimeout);
+  };
+
+  const handlePauseClick = () => {
+    disableTimer();
+
+    setIsPaused(!isPaused);
+  };
+
+  const handleNextClick = () => {
+    disableTimer();
+
+    getSlide();
+  };
+
+  const pauseButtonText = isPaused ? "Play" : "Pause";
+
+  return (
+    <div className="container">
+      <Header />
+      <Searcher />
+      <div className="slideshow-instruments">
+        <select
+          className="slideshow-interval-selector"
+          value={slidesInterval}
+          onChange={handleTimeoutChange}
+        >
+          <option value="10000">10 seconds</option>
+          <option value="5000">5 seconds</option>
+          <option value="3000">3 seconds</option>
+        </select>
+        <input
+          type="button"
+          value={pauseButtonText}
+          className="slideshow-button"
+          onClick={handlePauseClick}
+        />
+        <input
+          type="button"
+          value="Next"
+          className="slideshow-button"
+          onClick={handleNextClick}
+        />
+      </div>
+      {!slideImage ? (
+        <LoadingIndicator loading={true} />
+      ) : (
+        <div className="imageDiv">
+          <img className="full-image" src={slideImage} />
+        </div>
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {
-        slideImage: state.slideImage,
-    }
-}
+  return {
+    slideImage: state.slideImage,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        getSlide: function() {
-            showSlide();
-        }
-    }
-}
+  return {
+    getSlide: function () {
+      showSlide();
+    },
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SlideShow)
+export default connect(mapStateToProps, mapDispatchToProps)(SlideShow);
